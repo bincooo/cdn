bc300f2bdb5e01dc3d21afcbdcec3b496b387747
折戟沉沙、丿 <36452456+bincooo@users.noreply.github.com>
2022-12-31 23:32:57 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,17 @@
 (function() {
   'use strict'
-  window.___call = (name, args) => {
-    console.log(name, args, window[name])
-    window[name].call(args)
-  }
+  let intervalId = null
+  intervalId = setInterval(() => {
+    if (document.body.hasAttribute(data-position)) {
+      const jsonString = document.body.getAttribute('data-position')
+      cosnt position = JSON.parse(jsonString)
+      if (captchaSolverCall) {
+        captchaSolverCall({
+          x: position.left + 19,
+          y: position.top + 19
+        })
+        clearInterval(intervalId)
+      }
+    }
+  }, 1200)
 })()
